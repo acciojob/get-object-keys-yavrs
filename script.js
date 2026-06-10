c7c65547b1f57eefb7ff9765117a24546afbb27c
yavrs <yavrs786@gmail.com>
2026-06-10 12:30:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// Create the student object
+const student = {
+  name: "John",
+	age:23,
+	"full name":"john clark"
+};
+
+function getKeys(student){
+	return Objects.keys(student)
+}
+
+console.log(getKeys(student))
